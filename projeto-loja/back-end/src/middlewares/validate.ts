ed18a5d3970aa990dd9d971655efcc7ac32baca9
Igor Carvalho <igor.carvalhox8@gmail.com>
2022-08-08 18:04:01 -0300

    update

--- projeto-loja/back-end/src/middlewares/validate.ts
+++ projeto-loja/back-end/src/middlewares/validate.ts
@@ -2,6 +2,5 @@ import { Request, Response, NextFunction } from 'express';
 
 export const tokenValidate = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.csrfToken());
-  console.log('Passei pelo middleware');
   next();
 }
